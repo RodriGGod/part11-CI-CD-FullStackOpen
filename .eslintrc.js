39af9f08ba943d06c0b9f9f00c2a91957cd5a0f5
RodriGGod <rblo2002@gmail.com>
2025-10-22 16:45:29 +0200

    11.6 eslint.js modified

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,18 @@
+
+
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
+  env: { browser: true, node: true, es2021: true, jest: true },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: { ecmaVersion: 2021, sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  rules: {
+    quotes: ['error', 'single'],                 
+    'no-console': ['warn', { allow: ['warn', 'error'] }]
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  "plugins": [
-    "react", "jest"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
-  }
-}
+  overrides: [
+    {
+      files: ['*.config.js', '*.config.cjs', 'webpack.config.js', 'jest.config.js', '.eslintrc.js'],
+      env: { node: true }                        
+    }
+  ]
+};
