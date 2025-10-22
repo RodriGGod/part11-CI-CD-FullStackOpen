1982fc0f7a0b608a20f693aefb2ddb9cf599e445
RodriGGod <rblo2002@gmail.com>
2025-10-22 16:49:59 +0200

    11.6 eslint.js modified

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,30 @@
-
-
+/* eslint-env node */
+// eslint-disable-next-line no-console
 module.exports = {
   env: { browser: true, node: true, es2021: true, jest: true },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: { ecmaVersion: 2021, sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
   settings: { react: { version: 'detect' } },
   rules: {
-    quotes: ['error', 'single'],                 
-    'no-console': ['warn', { allow: ['warn', 'error'] }]
+    quotes: ['error', 'single'],
+    'react/react-in-jsx-scope': 'off', // React 17+ no requiere import React
+    'no-console': ['error', { allow: ['warn', 'error'] }]
+  },
+  globals: {
+    process: 'readonly' // si en el frontend hay process.env, evita no-undef
   },
   overrides: [
     {
       files: ['*.config.js', '*.config.cjs', 'webpack.config.js', 'jest.config.js', '.eslintrc.js'],
-      env: { node: true }                        
+      env: { node: true }
+    },
+    {
+      files: ['**/*.test.*', '**/__tests__/**'],
+      env: { jest: true }
     }
   ]
 };
