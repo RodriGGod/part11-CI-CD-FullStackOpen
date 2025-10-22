1f9ad4764b1ab3ba794b995679a2aeffcab73e4d
RodriGGod <rblo2002@gmail.com>
2025-10-22 19:21:17 +0200

    11.9 cypress

---
+++ e2e-tests/pokedex.cy.js
@@ -0,0 +1,14 @@
+describe('Pokedex', () => {
+  it('front page can be opened', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can navigate to a specific pokemon', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.contains('chlorophyll') // ojo: minúsculas en el source
+  })
+})
