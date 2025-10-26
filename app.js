71a61ce696068c0e61b5978d9bd684bc9cb609a3
RodriGGod <rblo2002@gmail.com>
2025-10-26 11:54:44 +0100

    11.11 render

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-// eslint-disable-next-line no-console
+
 const express = require('express');
 const app = express();
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
 
